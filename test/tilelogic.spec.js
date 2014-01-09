c1cb02cf0c06f2a5e371acc294a3a2664fa1b7bd
Aaron Blondeau <aaronblondeau@gmail.com>
2014-01-09 13:36:13 -0700

    use 6 decimal places in tests to prevent rounding issues form breaking tests

--- test/tilelogic.spec.js
+++ test/tilelogic.spec.js
@@ -5,11 +5,11 @@ if( typeof tilelogic === 'undefined' ) {
 describe('tilelogic', function(){
 
   it('x values convert to longitude', function(){
-    expect( tilelogic.tile2lng(3368, 14) ).toBe(-105.99609375);
+    expect( tilelogic.tile2lng(3368, 14).toFixed(6) ).toBe((-105.996094).toFixed(6));
   })
 
   it('y values convert to latitude', function(){
-    expect( tilelogic.tile2lat(6288, 14) ).toBe(38.54816542304658);
+    expect( tilelogic.tile2lat(6288, 14).toFixed(6) ).toBe((38.548165).toFixed(6));
   })
 
 })
